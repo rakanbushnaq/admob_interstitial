869e90773a387f4ed48fbe267093de9bc977515b
rakanbushnaq <rakanbushnaq@gmail.com>
2024-02-09 22:42:17 +0300

    e.dart

--- category_service.dart
+++ category_service.dart
@@ -1,12 +1,2 @@
 
-import 'package:kids_stories/repository/repository.dart';
-
-class CategoryService {
-  Repository? _repository;
-  CategoryService() {
-    _repository = Repository();
-  }
-  getCategories() async {
-    return await _repository?.httpGet('categories');
-  }
-}
+import 'package:kids_stories/reposito
